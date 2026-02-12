22d1aeb1a7b28ed27bc09e6e4e1ab0315433f654
Tony Travis <tony.travis12@gmail.com>
2026-02-12 23:26:50 +0000

    added shooting

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from logger import log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -20,6 +21,8 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
